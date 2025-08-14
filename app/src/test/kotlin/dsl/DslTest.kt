416047257a4c8f42d8de74907db411a5249461d2
sunghyun <puresprout@gmail.com>
2025-08-14 21:10:44 +0900

    dsl render 추가

--- app/src/test/kotlin/dsl/DslTest.kt
+++ app/src/test/kotlin/dsl/DslTest.kt
@@ -2,47 +2,80 @@ package com.purestation.app.dsl
 
 import kotlin.test.Test
 
+@DslMarker
+annotation class HtmlDsl
+
+interface Element {
+    fun render(): String
+}
+
+@HtmlDsl
+class Text(private val raw: String) : Element {
+    override fun render() = raw
+}
+
+@HtmlDsl
+class Ul : Element {
+    private val children = mutableListOf<Element>()
+
+    fun li(text: String) {
+        children += Text("<li>$text</li>")
+    }
+
+    override fun render() = "<ul>\n${children.joinToString("") { it.render() + "\n" }}</ul>"
+}
+
+@HtmlDsl
+class Body : Element {
+    private val children = mutableListOf<Element>()
+
+    fun h1(text: String) {
+        children += Text("<h1>$text</h1>\n")
+    }
+
+    fun p(text: String) {
+        children += Text("<p>$text</p>\n")
+    }
+
+    fun ul(block: Ul.() -> Unit) {
+        children += Ul().apply(block)
+    }
+
+    override fun render() = "<body>\n${children.joinToString("") { it.render() }}\n</body>"
+}
+
+@HtmlDsl
+class Html {
+    private lateinit var body: Body
+
+    fun body(block: Body.() -> Unit) {
+        body = Body().apply(block)
+    }
+
+    fun render() = "<html>\n${body.render()}\n</html>"
+}
+
+fun html(block: Html.() -> Unit) = Html().apply(block).render()
+
 class DslTest {
-    @DslMarker
-    annotation class HtmlDsl
-
-    @HtmlDsl
-    class Ul {
-        fun li(text: String) { println("<li>$text</li>") }
-    }
-
-    @HtmlDsl
-    class Body {
-        fun p(text: String) { println("<p>$text</p>") }
-        fun ul(block: Ul.() -> Unit) {
-            Ul().apply(block)
-        }
-    }
-
-    @HtmlDsl
-    class Html {
-        fun body(block: Body.() -> Unit) {
-            Body().apply(block)
-        }
-    }
-
-    fun html(block: Html.() -> Unit) {
-        Html().apply(block)
-    }
-
     @Test
     fun testHtmlDsl() {
-        html {
+        val page = html {
             body {
-                p("HTML DSL")
+                h1("HTML DSL")
+                p("There are many languages.")
                 ul {
-                    li("html")
-                    li("body")
-                    li("p")
-                    li("ul")
-                    li("li")
+                    li("kotlin")
+                    li("java")
+                    li("python")
+
+//                    body {
+//                        p("invalid body")
+//                    }
                 }
             }
         }
+
+        println(page)
     }
 }
